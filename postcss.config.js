b492e2789d4a8be293fc1d145134bd84a298579e
Rickard Laurin <rickard.laurin@gmail.com>
2019-09-06 14:31:58 +0200

    chore: add postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
-};
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./lib/**/*.js"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
 
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
+};
